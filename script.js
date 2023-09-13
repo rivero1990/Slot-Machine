f08bcf4fce9994d9f6a9487a4396107653109c29
Anibal Rivero <riveroleonel@gmail.com>
2023-09-13 05:57:28 -0300

    funcionalidad de la maquina tragamonedas

--- script.js
+++ script.js
@@ -1,147 +1,173 @@
-const parrafoDevcoins = document.querySelector("#devcoins");
+const paragraphDevcoins = document.querySelector("#devcoins");
 const checkboxInputs = document.querySelectorAll("input[name='cantidad']");
-const comprarBoton = document.querySelector("button");
-const saldoDevcoins = document.querySelector("#saldo");
+const buyBoton = document.querySelector("button");
+const balanceDevcoins = document.querySelector("#saldo");
 const reels = document.querySelectorAll(".reel");
 const botonSlot = document.querySelector("#boton-slot");
 const doors = document.querySelectorAll('.door');
+const profitsElement = document.querySelector("#ganancias");
 
+const FIRST_MINOR = 750;
+const JACKPOT = 4500;
+
+let profits = 0;
 
 function actualizarCantidadMonedas() {
-    let totalDevcoins = 0;
+  
+  let totalDevcoins = 0;
 
-    checkboxInputs.forEach(function(checkbox) {
-        if (checkbox.checked) {
-            totalDevcoins = parseInt(checkbox.value);
+  checkboxInputs.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      totalDevcoins = parseInt(checkbox.value);
 
-            checkboxInputs.forEach(function(checkbox) {
-                checkbox.disabled = true; 
-            });
-        }
-    });
+      checkboxInputs.forEach(function (checkbox) {
+        checkbox.disabled = true;
+      });
 
-    parrafoDevcoins.textContent = totalDevcoins;
-    actualizarCantidadSaldo(totalDevcoins);
+      buyBoton.disabled = true; 
+    }
+  });
+
+  paragraphDevcoins.textContent = totalDevcoins;
+  actualizarCantidadSaldo(totalDevcoins);
 }
 
+
 function actualizarCantidadSaldo(devcoins) {
-    saldoEnPesos = devcoins * 50; 
-    saldoDevcoins.textContent = "$ " + saldoEnPesos;
+  saldoEnPesos = devcoins * 50;
+  balanceDevcoins.textContent = "$ " + saldoEnPesos;
 }
 
 (function () {
-    const items = [
-      '🍎',
-      '🍒',
-      '🦁',
-      '🍊',
-      '7️⃣',
-      '🐘',
-      '🍋',
-      '🐴',
-      '🍇',
-      '💎',
-      '🍦',
-    ];
+  const items = ["🍎", "🍒", "🦁", "🍊", "🐘", "🍋", "🐴", "💎", "🍦", "🐒", "🦒", "🍐", "🍌", "🐊", "🍕"];
     
-    document.querySelector('#spinner').addEventListener('click', spin);
+
+  document.querySelector('#spinner').addEventListener('click', spin);
+
+  function init(firstInit = true, groups = 3, duration = 1) {
+    for (const door of doors) {
+      if (firstInit) {
+        door.dataset.spinned = '0';
+      } else if (door.dataset.spinned === '1') {
+        return;
+      }
+
+      const boxes = door.querySelector('.boxes');
+      const boxesClone = boxes.cloneNode(false);
+      const pool = [];
+
+      if (!firstInit) {
+        const arr = [];
+        for (let n = 0; n < (groups > 0 ? groups : 3); n++) {
+          arr.push(...items);
+        }
+        pool.push(...shuffle(arr));
+
+        boxesClone.addEventListener(
+          'transitionstart',
+          function () {
+            door.dataset.spinned = '1';
+            this.querySelectorAll('.box').forEach((box) => {
+              box.style.filter = 'blur(1px)';
+            });
+          },
+          { once: true }
+        );
+
+        boxesClone.addEventListener(
+          'transitionend',
+          function () {
+            this.querySelectorAll('.box').forEach((box, index) => {
+              box.style.filter = 'blur(0)';
+              if (index > 0) this.removeChild(box);
+            });
+          },
+          { once: true }
+        );
+      }
+
+      for (let i = pool.length - 1; i >= 0; i--) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.width = door.clientWidth + 'px';
+        box.style.height = door.clientHeight + 'px';
+        box.textContent = pool[i];
+        boxesClone.appendChild(box);
+      }
+      boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+      boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
+      door.replaceChild(boxesClone, boxes);
+    }
+  }
+
+  async function spin() {
+    let devcoins = parseInt(paragraphDevcoins.textContent);
+    let saldoActual = parseFloat(balanceDevcoins.textContent.replace('$ ', ''));
+    let premio = 0;
   
-    function init(firstInit = true, groups = 3, duration = 1) {
+    if (devcoins > 0) { 
       for (const door of doors) {
-        if (firstInit) {
-          door.dataset.spinned = '0';
-        } else if (door.dataset.spinned === '1') {
-          return;
-        }
+        door.dataset.spinned = '0';
+      }
   
-        const boxes = door.querySelector('.boxes');
-        const boxesClone = boxes.cloneNode(false);
-        const pool = [];
+      init(false, 1, 2);
   
-        if (!firstInit) {
-          const arr = [];
-          for (let n = 0; n < (groups > 0 ? groups : 3); n++) {
-            arr.push(...items);
-          }
-          pool.push(...shuffle(arr));
+      for (const door of doors) {
+        let boxes = door.querySelector('.boxes');
+        let duration = parseInt(boxes.style.transitionDuration);
+        boxes.style.transform = 'translateY(0)';
+        await new Promise((resolve) => setTimeout(resolve, duration * 100));
+      }
   
-          boxesClone.addEventListener(
-            'transitionstart',
-            function () {
-              door.dataset.spinned = '1';
-              this.querySelectorAll('.box').forEach((box) => {
-                box.style.filter = 'blur(1px)';
-              });
-            },
-            { once: true }
-          );
+      paragraphDevcoins.textContent = devcoins - 1; 
+      saldoActual -= 50;
+      balanceDevcoins.textContent = "$ " + saldoActual.toFixed(2);
   
-          boxesClone.addEventListener(
-            'transitionend',
-            function () {
-              this.querySelectorAll('.box').forEach((box, index) => {
-                box.style.filter = 'blur(0)';
-                if (index > 0) this.removeChild(box);
-              });
-            },
-            { once: true }
-          );
-        }
+      await new Promise((resolve) => setTimeout(resolve, 3000));
   
-        for (let i = pool.length - 1; i >= 0; i--) {
-          const box = document.createElement('div');
-          box.classList.add('box');
-          box.style.width = door.clientWidth + 'px';
-          box.style.height = door.clientHeight + 'px';
-          box.textContent = pool[i];
-          boxesClone.appendChild(box);
-        }
-        boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
-        boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
-        door.replaceChild(boxesClone, boxes);
+      let symbols = [
+        doors[0].querySelector('.box').textContent.trim(),
+        doors[1].querySelector('.box').textContent.trim(),
+        doors[2].querySelector('.box').textContent.trim()
+      ];
+  
+      if (symbols[0] === symbols[1] && symbols[0] === symbols[2]) {
+        premio = JACKPOT;
+      } else if (symbols[0] === symbols[1] || symbols[0] === symbols[2] || symbols[1] === symbols[2]) {
+        premio = FIRST_MINOR;
+      }
+  
+      if (premio > 0) {
+        profits += premio;
+        saldoActual += premio;
+        balanceDevcoins.textContent = "$ " + saldoActual.toFixed(2);
+        document.getElementById("ganancias").textContent = "$ " + profits.toFixed(2);
       }
     }
-  
-    async function spin() {
-  const devcoins = parseInt(parrafoDevcoins.textContent);
-  const saldoActual = parseFloat(saldoDevcoins.textContent.replace('$ ', ''));
-
-  if (devcoins > 0) {
-    init(false, 1, 2);
-
-    for (const door of doors) {
-      const boxes = door.querySelector('.boxes');
-      const duration = parseInt(boxes.style.transitionDuration);
-      boxes.style.transform = 'translateY(0)';
-      await new Promise((resolve) => setTimeout(resolve, duration * 100));
-    }
-
-    // Restar una Devcoin y $50 del saldo
-    parrafoDevcoins.textContent = devcoins - 1;
-    saldoDevcoins.textContent = "$ " + (saldoActual - 50).toFixed(2);
-    
-    setTimeout(init, 3000);
   }
   
-}
-
-    
-  
-    function shuffle([...arr]) {
-      let m = arr.length;
-      while (m) {
-        const i = Math.floor(Math.random() * m--);
-        [arr[m], arr[i]] = [arr[i], arr[m]];
-      }
-      return arr;
+  function shuffle([...arr]) {
+    let m = arr.length;
+    while (m) {
+      const i = Math.floor(Math.random() * m--);
+      [arr[m], arr[i]] = [arr[i], arr[m]];
     }
-  
-    init();
-  })();
+    return arr;
+  }
+
+  init();
+})();
 
 
 
-  
+
+
+
+
+
+
+
+
 
 
 
